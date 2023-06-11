2a33c1cc0af4d96f27db045b90d303832a86c4bf
Genesee Harmon <geneseelh@gmail.com>
2023-06-11 14:24:25 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import ShowDetail from './ShowDetail';
 
 function App() {
   const [shows, setShows] = useState([]);
+
   useEffect(() => {
     fetch("https://api.tvmaze.com/shows?page=1")
       .then(response => response.json())
-      .then(data => setShows(data));
-  })
+      .then(data => setShows(data))
+      .catch((err)=>{
+        console.log(err)
+      });
+  }, []);
+  //console.log(shows[0])
+
+
   return (
     <div className="App">
       <h1>Look at these TV shows!</h1>
+      {shows.map((show, index)=>{
+        return (
+          <ShowDetail key={index} show={show}/>
+        )
+      })}
     </div>
   );
 }
--- src/ShowDetail.js
+++ src/ShowDetail.js
@@ -1,1 +1,48 @@
+import React, {useState, useEffect} from "react";
 
+function ShowDetail({show}) {
+    const [imgClicked, setImgClicked] = useState(false);
+    const [titleClicked, setTitleClicked] = useState(false);
+    const [akas, setAkas] = useState(null);
+    
+
+    function imageClickHandler(){
+        setImgClicked(!imgClicked)
+      }
+    function titleClickHandler(){
+        setTitleClicked(!titleClicked)
+    }
+
+    useEffect(()=>{
+        if(titleClicked){
+        fetch(`https://api.tvmaze.com/shows/${show.id}/akas`)
+            .then(response => response.json())
+            .then(data => setAkas(data))
+            .catch(err => {console.log(err)});   
+        } else if(!titleClicked){
+            setAkas(null)
+        }
+        //console.log(show.id)
+    }, [titleClicked]);
+    
+  return (
+    <div>
+        <h3 className="title" onClick={titleClickHandler}>{show.name}</h3> 
+        {akas ? 
+            <div>
+                {akas.map((aka, index)=>{
+                    return <div key={index}>{aka.name}<br/>{aka.country.name}</div>
+                })}
+            </div>
+        : null}
+        <img src={show.image.medium} onClick={imageClickHandler}/>
+        {imgClicked ? 
+            <div>
+                {show.type}<br/>{show.status}
+            </div> 
+        : null}
+    </div>
+  )
+}
+
+export default ShowDetail;
